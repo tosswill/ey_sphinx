720dca66f58c52ef950cf91488c18c99ca2bcb7c
Chris Tosswill <ctosswill@engineyard.com>
2011-06-13 16:19:20 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EySphinx::Application.config.secret_token = 'f3d959adefd99c1ea5442a86fb274ac6a1f33a36fb2ff529263011ef6c169588d654d1df2d1e497a453c5543939f82ea2f8a8275fe56ece98942e84674198b84'
