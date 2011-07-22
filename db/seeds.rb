57a8ee385a2cad83e0bbef950a6a8f71c92031e5
Chris Tosswill <ctosswill@engineyard.com>
2011-07-22 14:37:35 -0700

    made updates for screencast

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+Story.create!(:title => 'Robinson Crusoe', :body =>  "Crusoe (the family name corrupted from the German name 'Kreutznaer') sets sail from the Queen's Dock in Hull on a sea voyage in August 1651, against the wishes of his parents, who want him to stay at home and pursue a career, possibly in law. After a tumultuous journey that sees his ship wrecked in a storm, his lust for the sea remains so strong that he sets out to sea again.")
+
+Story.create!(:title => "The Adventures of Sherlock Holmes", :body => "The Adventures of Sherlock Holmes is a collection of twelve stories by Arthur Conan Doyle, featuring his famous detective and illustrated by Sidney Paget." )
+
+Story.create!(:title => "The Call of the Wild", :body => "Buck, a powerful Saint Bernard-Shepherd dog, lives a comfortable life in the Santa Clara Valley with his owner, Judge Miller. One day, Manuel, the Judge's gardener's assistant, steals Buck and sells him in order to pay a gambling debt. Buck is then shipped to the 'man in the red sweater' to be broken. " )
+
+Story.create!(:title => "The Tempest", :body => "The magician Prospero, rightful Duke of Milan, and his daughter, Miranda, have been stranded for twelve years on an island after Prospero's jealous brother Antonio—helped by Alonso, the King of Naples—deposed him and set him adrift with the then three-year-old Miranda." )
+
+Story.create!(:title => "The Adventures of Tom Sawyer", :body => "Tom Sawyer lives with his Aunt Polly, brother Sid, and cousin Mary. He is a mischievous boy, disinterested in school, and continually recruiting his equally-mischievous friends to join him on his adventures." )
